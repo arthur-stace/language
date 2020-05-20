aa48a62235b3aec96fb444bda63a210f96bf716e
Arthur Stace <arthur.stace@pm.me>
2020-05-20 11:43:30 +1000

    Squashed 'makefiles/' content from commit 6291651
    git-subtree-dir: makefiles
    git-subtree-split: 62916515177edb5bae9df5b1cabc6f17666255c9

---
+++ ocw.mit.edu.mk
@@ -0,0 +1,23 @@
+OCW_ZIP_DOMAIN = https://ocw.mit.edu/ans15436/ZipForEndUsers
+
+tmp/notes:
+	mkdir -p $@
+
+tmp/${COURSE}:
+	mkdir -p $@
+
+
+tmp/${COURSE}.zip: tmp/${COURSE}
+	curl --output $@ \
+		${OCW_ZIP_DOMAIN}/${COURSE_PREFIX}/${COURSE}/${COURSE}.zip
+	tar xf $@ -C $<
+
+.PHONY:
+
+
+tmp/%.txt: tmp/${COURSE}.zip .PHONY
+	cat tmp/${COURSE}/${COURSE}/contents/$*/index.htm \
+	| pup "#course_inner_section" \
+	| lynx -list_inline -dump -stdin \
+	| makefiles/ocw.mit.edu/$*.sh
+
